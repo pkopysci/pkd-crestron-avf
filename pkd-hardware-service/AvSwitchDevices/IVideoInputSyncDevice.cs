65e63a4314affe4cc515fca26069b4245f09e135
Philip J Kopyscinski <phil.kopysci@gmail.com>
2025-07-21 12:38:12 -0700

    Create IVideoInputSyncDevice.cs
    Created the public interface for adding video input sync support to a AVR device. The application service still needs to be updated to act on devices that implement this interface.

---
+++ pkd-hardware-service/AvSwitchDevices/IVideoInputSyncDevice.cs
@@ -0,0 +1,21 @@
+﻿using pkd_common_utils.GenericEventArgs;
+
+namespace pkd_hardware_service.AvSwitchDevices;
+
+/// <summary>
+/// Required events, methods, and properties for any AV switcher that supports video input sync monitoring.
+/// </summary>
+public interface IVideoInputSyncDevice
+{
+    /// <summary>
+    /// Arg = the input number that is reporting a sync status changed.
+    /// </summary>
+    event EventHandler<GenericSingleEventArgs<uint>> VideoInputSyncStateChanged;
+    
+    /// <summary>
+    /// Query the state of the input sync as last reported by the device.
+    /// </summary>
+    /// <param name="input">the index of the input to query</param>
+    /// <returns>true if the input has sync, false otherwise.</returns>
+    bool QueryVideoInputSyncState(uint input);
+}
